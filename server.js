e7fc01df184314b0f6806abe39503126bcb8170a
Emmanuel <diomemmanuel01@gmail.com>
2026-04-02 20:07:44 +0000

    ajuster prt pour render

--- server.js
+++ server.js
@@ -131,4 +131,7 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("🚀 Serveur démarré sur http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
